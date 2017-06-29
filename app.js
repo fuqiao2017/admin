9e9b2d980e22c424f83ed7c638c77c6c3fc5f962
fuqiao2017 <270217737@qq.com>
2017-06-29 21:27:03 +0800

    服务器端初次搭建

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 var express = require('express');
 var app = express();
 
+//allow custom header and CORS
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:9999');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+    if (req.method == 'OPTIONS') {
+        res.send(200); //让options请求快速返回
+    } else {
+        next();
+    }
+});
+
 app.get('/', function(req, res) {
     res.send('Hello World!');
 });
 
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!');
+});
+
+app.all('/secret', function(req, res, next) {
+    res.json({ success: '哈哈哈哈' });
 });
